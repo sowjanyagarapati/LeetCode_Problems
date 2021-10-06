8a970fa0f494f240ff175631b1e4ff2de5fc1d5a
sowjanyagarapati <46521587+sowjanyagarapati@users.noreply.github.com>
2021-10-06 12:28:41 +0530

    Update ContainsDuplicate.py

--- Arrays/ContainsDuplicate.py
+++ Arrays/ContainsDuplicate.py
@@ -1,1 +1,9 @@
+'''Author @Sowjanya '''
+''' This Program returns true if a list contains duplicates else returns false'''
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums)==len(set(nums)):
+            return False
+        else:
+            return True
