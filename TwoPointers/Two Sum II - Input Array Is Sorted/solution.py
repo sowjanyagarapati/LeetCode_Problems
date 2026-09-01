18c36c8ca2fa81c486dcbb8cfd422926841fb20c
lakshmi sowjanya <lgarapati@lakshmis-MacBook-Air.local>
2026-09-02 05:22:20 +0530

    added Two sum ii

---
+++ TwoPointers/Two Sum II - Input Array Is Sorted/solution.py
@@ -0,0 +1,11 @@
+## brute force O(n)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        pairMap = {}
+        for i,v in enumerate(numbers):
+            pair = target - v
+            if pair not in pairMap:
+                pairMap[v] = i+1
+            else:
+                return [pairMap.get(pair), i+1]
+        
