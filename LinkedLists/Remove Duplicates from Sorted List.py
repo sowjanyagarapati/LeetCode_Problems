b068f660c7a4100b6011aa578d00a1f54378025d
sowjanyagarapati <lakshmisowjanya.garapati@gmail.com>
2026-09-03 19:11:29 +0530

    added approach

--- LinkedLists/Remove Duplicates from Sorted List.py
+++ LinkedLists/Remove Duplicates from Sorted List.py
@@ -1,4 +1,15 @@
 # Definition for singly-linked list.
+
+'''Approach:
+current pointer should be initialized to head
+current.next is checked for non empty
+if current.val == current.next.val, then current.next is updated to current.next.next
+else, current pointer is moved to the next node
+
+Time complexity: O(n) in the worst case, where n is the length of the input list. Each pointer moves at most n times combined, so overall linear.
+
+Space complexity: O(1) extra space, since only a few variables are used regardless of input size.
+'''
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
